9a751d045a19e18395754c5152326c818d4d2cb8
MCMrARM <mcmrarm@gmail.com>
2018-05-24 17:58:06 +0200

    Find MSA UI executable relative to the app dir, support multiple paths

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,28 @@
 #include "MsaService.h"
 #include "MsaUiLauncher.h"
 #include "MsaUiHelper.h"
-#include <thread>
 #include <FileUtil.h>
 #include <EnvPathUtil.h>
+#include <log.h>
+
+static std::string findMsaUI() {
+#ifdef MSA_UI_APP_PATH
+    std::string path = MSA_UI_APP_PATH;
+    auto appDir = EnvPathUtil::getAppDir() + "/";
+    for (size_t iof = 0; iof != std::string::npos; ) {
+        size_t iof2 = path.find(':', iof);
+        std::string el = (iof2 == std::string::npos ? path.substr(iof) : path.substr(iof, iof2 - iof));
+        if (el[0] != '/')
+            el = appDir + el;
+        if (FileUtil::exists(el))
+            return el;
+        iof = iof2;
+    }
+#else
+    Log::warn("Main", "No Msa UI path set on compile-time; will not be able to add new accounts");
+#endif
+    return std::string();
+}
 
 int main() {
     std::string msaHome = EnvPathUtil::getDataHome() + "/msa";
@@ -11,7 +30,7 @@ int main() {
     std::string msaUiService = msaHome + "/.ui_service";
     std::string msaDataHome = msaHome + "/data";
     FileUtil::mkdirRecursive(msaDataHome);
-    MsaUiLauncher uiLauncher (MSA_UI_APP_PATH, msaUiService);
+    MsaUiLauncher uiLauncher (findMsaUI(), msaUiService);
     MsaUiHelper uiHelper (uiLauncher);
     MsaService service(msaService, msaDataHome, uiHelper);
 
