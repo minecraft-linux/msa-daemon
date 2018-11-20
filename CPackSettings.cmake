e795491a18360584cd2ef7376ad1a02afa76bf76
MCMrARM <mcmrarm@gmail.com>
2018-11-20 20:43:14 +0100

    Sensible versioning system for debian

--- CPackSettings.cmake
+++ CPackSettings.cmake
@@ -1,10 +1,12 @@
 set(CPACK_PACKAGE_NAME "msa-daemon")
 set(CPACK_PACKAGE_VENDOR "mcpelauncher")
+set(CPACK_PACKAGE_VERSION "${MANIFEST_GIT_COMMIT_HASH}")
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Microsoft Account login daemon")
 set(CPACK_PACKAGE_CONTACT "https://github.com/minecraft-linux/msa-manifest/issues")
 set(CPACK_GENERATOR "TGZ;DEB")
 set(CPACK_INSTALL_CMAKE_PROJECTS "${CMAKE_CURRENT_BINARY_DIR};msa-daemon;msa-daemon;/")
 set(CPACK_OUTPUT_CONFIG_FILE CPackConfig.cmake)
 set(CPACK_DEBIAN_PACKAGE_DEPENDS "libc6 (>=2.14), libssl1.1, libcurl4")
+set(CPACK_DEBIAN_PACKAGE_VERSION "${BUILD_TIMESTAMP}-${MANIFEST_GIT_COMMIT_HASH}")
 
 include(CPack)
